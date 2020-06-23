066597bf9e8265e72b7e497158986cd40f95a87c
Inácio Pires <viniciusinaciopires@gmail.com>
2020-06-23 12:24:07 -0300

    new: ServerList

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import ServerButton from "../ServerButton";
+
+import { Container, Separator } from "./styles";
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome />
+
+      <Separator />
+
+      <ServerButton />
+      <ServerButton mentions={3} />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton mentions={29} />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton mentions={29} />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton />
+    </Container>
+  );
+};
+
+export default ServerList;
